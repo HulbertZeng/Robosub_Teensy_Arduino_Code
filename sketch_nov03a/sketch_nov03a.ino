a56811c93d4a63b9e003a2991eb996f950cd0aaa
HulbertZeng <hulbert2013@gmail.com>
2021-11-16 17:51:48 -0800

    removed blinking unless change in value

--- sketch_nov03a/sketch_nov03a.ino
+++ sketch_nov03a/sketch_nov03a.ino
@@ -9,35 +9,42 @@
 TFT LCDScreen = TFT(cs, dc, rst);
 char printout[10];
 
-const int R5 = 6;
-const int R6 = 7;
+String batt1 = "";
+String batt2 = "";
+
+const int R5 = A6;
+const int R6 = A7;
 
 void setup() {
   // put your setup code here, to run once:
   LCDScreen.begin();
-  pinMode(R5, OUTPUT);
-  pinMode(R6, OUTPUT);
+  pinMode(R5, INPUT);
+  pinMode(R6, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  String batt1 = String(digitalRead(R5));
-  String batt2 = String(digitalRead(R6));
-
-  LCDScreen.background(0, 0, 0);      // clear screen
+  if(batt1 != String(analogRead(R5)) || batt2 != String(analogRead(R6))) {
+    batt1 = String(analogRead(R5));
+    batt2 = String(analogRead(R6));
+    LCDScreen.background(0, 0, 0);      // clear screen
+  }
+  
   LCDScreen.stroke(0, 255, 0);        // set to green
+  
   LCDScreen.setTextSize(2);           // set text size
   LCDScreen.text("Battery 1", 0, 0);  // print at (0, 0) coordinate
-
   batt1.toCharArray(printout, 10);   // add to printout
   LCDScreen.setTextSize(5);           // set text size
   LCDScreen.text(printout, 0, 20);    // print printout array
 
   LCDScreen.setTextSize(2);
   LCDScreen.text("Battery 2", 0, 60);
-
   batt2.toCharArray(printout, 10);
   LCDScreen.setTextSize(5);
   LCDScreen.text(printout, 0, 80);
+
+  Serial.println(analogRead(R6));
   delay(500);
 }
